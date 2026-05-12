9251a3bc00172f126a04d05a809de95db84c6cda
Diego Augusto <80727709+Diego0668@users.noreply.github.com>
2026-05-12 14:56:49 -0400

    Add files via upload

---
+++ src/model/Adega.java
@@ -0,0 +1,21 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Adega {
+
+    private Produto produto;
+    private Funcionario funcionario;
+    private Cliente cliente;
+
+    public Adega(Produto produto , Funcionario funcionario , Cliente cliente){
+        
+        this.produto = produto;
+        this.funcionario = funcionario;
+        this.cliente = cliente;
+    }
+
+
+    
+}
